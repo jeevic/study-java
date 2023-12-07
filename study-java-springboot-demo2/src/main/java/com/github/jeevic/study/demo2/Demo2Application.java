7a3334e4da398e58b36356a5a7b6036f5ac9230b
caojianwei-jk <caojianwei-jk@360shuke.com>
2023-12-07 21:31:57 +0800

    Add: add serverlet demo

--- study-java-springboot-demo2/src/main/java/com/github/jeevic/study/demo2/Demo2Application.java
+++ study-java-springboot-demo2/src/main/java/com/github/jeevic/study/demo2/Demo2Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 /**
  * 将 beans.xml 加载到项目中
@@ -16,7 +17,7 @@ import java.io.IOException;
 public class Demo2Application {
 
     public static void main(String[] args) {
-
+        System.out.println(LocalDate.now().toString());
         System.out.println("系统默认编码：" + System.getProperty("file.encoding"));
 
         byte[] byteData = new byte[100];
